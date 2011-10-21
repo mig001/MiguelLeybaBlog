312c9dfe4f9ba5fe1c41631d761c273a16b84f07
Miguel <mig@unm.edu>
2011-10-20 23:03:23 -0600

    entire project

---
+++ migBlogger/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = 'accbda7f9afd85af8e6bbc97345ecf1ae2f6db1a22b286792eddb3d1410fa90b81b46d0874c04d0c655239f17e7a3b063facabde5140a698cef83cbddefd1393'
